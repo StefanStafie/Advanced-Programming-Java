a4c981f618e63c1357772583a6b73259decb2610
Stefan Stafie <stefan_stafie@yahoo.com>
2020-03-19 14:53:32 +0200

    Lab5

---
+++ src/com/company/CatalogUtil.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CatalogUtil {
+    public static void save(Catalog catalog)
+            throws IOException {
+        try (var oos = new ObjectOutputStream(
+                new FileOutputStream(catalog.getPath()))) {
+            oos.writeObject(catalog);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static Catalog load(String path)
+            throws InvalidCatalogException {
+        try (var ois = new ObjectInputStream(new FileInputStream(path))) {
+            return (Catalog) ois.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void view(Document doc) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(new URI(doc.getLocation()));
+            desktop.edit(new File(doc.getLocation()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Main app = new Main();
+            app.testCreateSave();
+            app.testLoadView();
+        }
+
+        private void testCreateSave() {
+            Catalog catalog =
+                    new Catalog("Java Resources", "d:/catalog.ser");
+            Document doc = new Document("java1", "Java Course 1",
+                    "https://profs.info.uaic.ro/~acf/java/slides/en/intro_slide_en.pdf");
+
+            doc.addTag("type", "Slides");
+            catalog.add(doc);
+
+            doc = new Document("java2", "Java Course 1",
+                    "d:/catalog.ser");
+            catalog.add(doc);
+            try {
+                CatalogUtil.save(catalog);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        private void testLoadView() {
+            Catalog catalog = null;
+            try {
+                catalog = CatalogUtil.load("d:/catalog.ser");
+            } catch (InvalidCatalogException e) {
+                e.printStackTrace();
+            }
+            Document doc = catalog.findById("java2");
+            CatalogUtil.view(doc);
+            Document doc2 = catalog.findById("java1");
+            CatalogUtil.view(doc);
+        }
+    }
